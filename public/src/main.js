eedb12d060e26a399c6fb76f5f11b458b6856dc0
Tais <55287175+RocketBadger@users.noreply.github.com>
2023-05-14 17:42:34 +0200

    Test functions succesfully displaying to UI

--- public/src/main.js
+++ public/src/main.js
@@ -1,19 +1,26 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+if (typeof firebase === 'undefined')
+  throw new Error(
+    'hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js'
+  )
 
-const db = firebase.firestore();
+const db = firebase.firestore()
+
+
+//---------------------------- Test functions ----------------------------//
 
 const testbtn = document.getElementById('testbtn')
-testbtn.addEventListener('click', e => getRecords())
+testbtn.addEventListener('click', e => readRecords())
 
 const testNode = document.getElementById('test')
 
+
 async function test () {
-  const docRef = db.collection('users').doc('alovelace');
+  const docRef = db.collection('users').doc('alovelace')
   await docRef.set({
     first: 'Ada',
     last: 'Lovelace',
     born: 1815
-  });
+  })
   // try {
   //   const docRef = await firebase.addDoc(firebase.collection(db, 'users'), {
   //     first: 'Ada',
@@ -35,10 +42,13 @@ async function getRecords () {
   if (!doc.exists) {
     testNode.innerText = 'No such document!'
   } else {
-    testNode.innerText = 'TESTERBEDSTERFÆTTER'
+    testNode.innerText = 'recordsRead'
   }
 }
 
-function testfunct () {
-  testNode.innerText = 'TESTERBEDSTERFÆTTER'
+async function readRecords () {
+  const snapshot = await db.collection('users').get()
+  snapshot.forEach(doc => {
+    testNode.innerText = doc.id
+  })
 }
