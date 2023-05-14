049dff6cd937cc5b3f6e9ad45c341930c830b2cf
Tais <55287175+RocketBadger@users.noreply.github.com>
2023-05-14 13:15:58 +0200

    WE HAVE READ/WRITE DB ACCESS THIS IS NOT A DRILL

---
+++ public/src/main.js
@@ -0,0 +1,44 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+
+const db = firebase.firestore();
+
+const testbtn = document.getElementById('testbtn')
+testbtn.addEventListener('click', e => getRecords())
+
+const testNode = document.getElementById('test')
+
+async function test () {
+  const docRef = db.collection('users').doc('alovelace');
+  await docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
+  // try {
+  //   const docRef = await firebase.addDoc(firebase.collection(db, 'users'), {
+  //     first: 'Ada',
+  //     last: 'Lovelace',
+  //     born: 1815
+  //   })
+  //   console.log('Document written with ID: ', docRef.id)
+  // } catch (e) {
+  //   console.error('Error adding document: ', e)
+  // }
+  console.log('db tjek')
+}
+test()
+
+async function getRecords () {
+  testNode.innerText = 'entered function'
+  const usersRef = db.collection('users').doc('alovelace')
+  const doc = await usersRef.get()
+  if (!doc.exists) {
+    testNode.innerText = 'No such document!'
+  } else {
+    testNode.innerText = 'TESTERBEDSTERFÆTTER'
+  }
+}
+
+function testfunct () {
+  testNode.innerText = 'TESTERBEDSTERFÆTTER'
+}
